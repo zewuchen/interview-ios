3241d83c2106ed2bebd39a204040281a19efe0d7
Zewu Chen <zewu.chen@etec.sp.gov.br>
2024-06-28 12:42:07 -0300

    Add initial project

---
+++ Interview/Interview/Feature/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Interview
+//
+//  Created by Zewu Chen on 28/06/24.
+//
+
+import Foundation
+
+import UIKit
+
+public class DetailViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Interview/Interview/Feature/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Interview
+//
+//  Created by Zewu Chen on 28/06/24.
+//
+
+import UIKit
+
+public class MainViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
