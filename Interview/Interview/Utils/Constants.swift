326d1c2e52b656599c9f81f28f1fc10455d1e83d
Rafael Felipe <rafael.felipe1989@gmail.com>
2024-09-05 13:41:02 -0300

    Create Constants.swift

---
+++ Interview/Interview/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Interview
+//
+//  Created by Rafael Ramos on 05/09/24.
+//
+
+import Foundation
+
+struct Constants {
+    struct Space {
+        static let medium: CGFloat = 8
+    }
+}
