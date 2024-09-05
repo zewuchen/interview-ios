94adb1018203de7d7d0c67d7ac9e35c01115763f
Rafael Felipe <rafael.felipe1989@gmail.com>
2024-09-05 14:07:25 -0300

    Create NetworkerSpy.swift

---
+++ Interview/InterviewTests/Networker/Spies/NetworkerSpy.swift
@@ -0,0 +1,63 @@
+//
+//  NetworkerSpy.swift
+//  InterviewTests
+//
+//  Created by Rafael Ramos on 05/09/24.
+//
+
+import Foundation
+import XCTest
+@testable import Interview
+
+final class NetworkerSpy<R: Decodable>: NetworkerProtocol {
+    private(set) var requestVerifier: [Endpoint] = []
+    private(set) var requestDataVerifier: [URLEndpoint] = []
+    var resultToBeReturned: Result<R, NetworkerError>?
+    var resultDataToBeReturned: Result<Data, NetworkerError>?
+    
+    func request<T: Decodable>(
+        endpoint: Endpoint,
+        completion: @escaping ((Result<T, NetworkerError>) -> Void)
+    ) {
+        requestVerifier.append(endpoint)
+        guard let result = resultToBeReturned as? Result<T, NetworkerError> else {
+            XCTFail("resultToBeReturned must be casting with Result<T, NetworkerError>")
+            return
+        }
+        
+        completion(result)
+    }
+    
+    func requestData(
+        endpoint: URLEndpoint,
+        completion: @escaping ((Result<Data, NetworkerError>) -> Void)
+    ) {
+        requestDataVerifier.append(endpoint)
+        guard let result = resultDataToBeReturned else {
+            XCTFail("resultDataToBeReturned must be casting with Result<T, NetworkerError>")
+            return
+        }
+        
+        completion(result)
+    }
+    
+    func verifyArgumentsToRequest(
+        endpoint: Endpoint,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        XCTAssertEqual(requestVerifier.first?.baseUrl, endpoint.baseUrl, file: file, line: line)
+        XCTAssertEqual(requestVerifier.first?.host, endpoint.host, file: file, line: line)
+        XCTAssertEqual(requestVerifier.first?.method, endpoint.method, file: file, line: line)
+    }
+    
+    func verifyArgumentsToRequestData(
+        endpoint: URLEndpoint,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        XCTAssertEqual(requestDataVerifier.first?.url, endpoint.url, file: file, line: line)
+        XCTAssertEqual(requestDataVerifier.first?.host, endpoint.host, file: file, line: line)
+        XCTAssertEqual(requestDataVerifier.first?.method, endpoint.method, file: file, line: line)
+    }
+}
