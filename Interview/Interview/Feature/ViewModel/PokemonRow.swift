f4d92c05822c2ac6c37dd862d19a73cbef34fac5
Rafael Felipe <rafael.felipe1989@gmail.com>
2024-09-05 13:41:08 -0300

    Create PokemonRow.swift

---
+++ Interview/Interview/Feature/ViewModel/PokemonRow.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonRow.swift
+//  Interview
+//
+//  Created by Rafael Ramos on 05/09/24.
+//
+
+import Foundation
+import UIKit
+
+struct PokemonRow {
+    var title: String?
+    var background: UIColor
+}
