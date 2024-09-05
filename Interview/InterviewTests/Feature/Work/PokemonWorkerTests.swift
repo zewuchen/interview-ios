1c58bb9b8d0a80c7016ad5cbcbe88c9e9923e8d6
Rafael Felipe <rafael.felipe1989@gmail.com>
2024-09-05 14:07:28 -0300

    Create PokemonWorkerTests.swift

---
+++ Interview/InterviewTests/Feature/Work/PokemonWorkerTests.swift
@@ -0,0 +1,64 @@
+//
+//  PokemonWorkerTests.swift
+//  InterviewTests
+//
+//  Created by Rafael Ramos on 05/09/24.
+//
+
+import XCTest
+@testable import Interview
+
+final class CharactersWorkerTests: XCTestCase {
+
+    private let networkerSpy: NetworkerSpy<PokemonCatalogResponse> = .init()
+    private let environmentStub: EnvironmentStub.Type = EnvironmentStub.self
+    private lazy var sut: PokemonWorker = PokemonWorker(
+        networker: networkerSpy,
+        environment: environmentStub
+    )
+
+    func test_fetchCharacters_whenSuccess_shouldCallRequest() throws {
+        networkerSpy.resultToBeReturned = .success(
+            PokemonCatalogResponse(
+                count: 1,
+                results: [.init(name: "dummyName", url: "dummyUrl")]
+            )
+        )
+        
+        sut.fetchPokemons { result in
+            XCTAssertEqual(
+                result,
+                .success(
+                    PokemonCatalogResponse(
+                        count: 1,
+                        results: [.init(name: "dummyName", url: "dummyUrl")]
+                    )
+                )
+            )
+        }
+        
+        networkerSpy.verifyArgumentsToRequest(
+            endpoint: DummyEndpoint(
+                host: "https://pokeapi.co",
+                baseUrl: "/api/v2/pokemon/",
+                method: .GET
+            )
+        )
+    }
+    
+    func test_f1etchCharacters_whenSuccess_shouldCallRequest() throws {
+        networkerSpy.resultToBeReturned = .failure(.badRequest)
+        
+        sut.fetchPokemons { result in
+            XCTAssertEqual(result, .failure(.badRequest))
+        }
+        
+        networkerSpy.verifyArgumentsToRequest(
+            endpoint: DummyEndpoint(
+                host: "https://pokeapi.co",
+                baseUrl: "/api/v2/pokemon/",
+                method: .GET
+            )
+        )
+    }
+}
