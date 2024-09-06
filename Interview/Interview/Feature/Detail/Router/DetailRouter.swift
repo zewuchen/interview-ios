2e5d86607774f975664387c259b0176ad652ecc7
Rafael Felipe <rafael.felipe1989@gmail.com>
2024-09-06 20:51:31 -0300

    Create DetailRouter.swift

---
+++ Interview/Interview/Feature/Detail/Router/DetailRouter.swift
@@ -0,0 +1,27 @@
+//
+//  DetailRouter.swift
+//  Interview
+//
+//  Created by Rafael Ramos on 05/09/24.
+//
+
+import Foundation
+import UIKit
+
+final class DetailRouter {
+    static func show(from navigation: UINavigationController?, with url: URL) {
+        let mathHelper: MathHelper = .init()
+        let pokemonDetailAssetRuleUseCase: PokemonDetailAssetRuleUseCase = PokemonDetailAssetRuleUseCase(
+            mathHelper: mathHelper
+        )
+        let viewModel: DetailViewModel = .init(
+            pokemonDetailWorker: PokemonWorkerFactory.make(),
+            url: url,
+            pokemonDetailAssetRuleUseCase: pokemonDetailAssetRuleUseCase
+        )
+        
+        let controller: DetailViewController = .init(viewModel: viewModel)
+        
+        navigation?.pushViewController(controller, animated: true)
+    }
+}
