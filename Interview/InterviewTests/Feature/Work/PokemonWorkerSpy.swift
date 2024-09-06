c81a03d3171c2c447672c85ca7b24240ad031838
Rafael Felipe <rafael.felipe1989@gmail.com>
2024-09-06 20:29:26 -0300

    Update PokemonWorkerSpy.swift

--- Interview/InterviewTests/Feature/Work/PokemonWorkerSpy.swift
+++ Interview/InterviewTests/Feature/Work/PokemonWorkerSpy.swift
@@ -10,11 +10,19 @@ import XCTest
 @testable import Interview
 
 final class PokemonWorkerSpy: PokemonWorkerProtocol {
-    
     private(set) var fetchPokemonsVerifier: Int = 0
-    var resultToBeReturned: Result<PokemonCatalogResponse, NetworkerError> = .failure(.badRequest)
+    private(set) var fetchPokemonDetailVerifier: [URL] = []
+    
+    var resultFetchPokemonsToBeReturned: Result<PokemonCatalogResponse, NetworkerError> = .failure(.badRequest)
+    var resultFetchPokemonDetailToBeReturned: Result<PokemonDetailResponse, NetworkerError> = .failure(.badRequest)
     
     func fetchPokemons(completion: @escaping ((Result<PokemonCatalogResponse, NetworkerError>) -> Void)) {
-        completion(resultToBeReturned)
+        fetchPokemonsVerifier += 1
+        completion(resultFetchPokemonsToBeReturned)
+    }
+    
+    func fetchPokemonDetail(url: URL, completion: @escaping ((Result<PokemonDetailResponse, NetworkerError>) -> Void)) {
+        fetchPokemonDetailVerifier.append(url)
+        completion(resultFetchPokemonDetailToBeReturned)
     }
 }
