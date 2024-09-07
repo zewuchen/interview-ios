911ebdfd4037972ffef61ab3ab2f55ebc2bbd451
Rafael Felipe <rafael.felipe1989@gmail.com>
2024-09-07 17:45:47 -0300

    Update Networker.swift

--- Interview/Interview/Network/Networker.swift
+++ Interview/Interview/Network/Networker.swift
@@ -11,6 +11,7 @@ protocol NetworkerProtocol {
     func request<T: Decodable>(
         endpoint: Endpoint,
         cachePolicy: URLRequest.CachePolicy,
+        cacheWorker: CacheworkerProtocol,
         completion: @escaping ((Result<T, NetworkerError>) -> Void)
     )
 }
@@ -28,14 +29,19 @@ final class Networker: NetworkerProtocol {
         self.jsonNDecoder = jsonNDecoder
     }
     
-    func request<T: Decodable>(endpoint: Endpoint, cachePolicy: URLRequest.CachePolicy, completion: @escaping ((Result<T, NetworkerError>) -> Void)) {
+    func request<T: Decodable>(
+        endpoint: Endpoint,
+        cachePolicy: URLRequest.CachePolicy,
+        cacheWorker: CacheworkerProtocol,
+        completion: @escaping ((Result<T, NetworkerError>) -> Void)
+    ) {
         guard let urlRequest = URLRequestFactory.make(from: endpoint, cachePolicy: cachePolicy) else {
             completion(.failure(.unknown("URLRequest is nil")))
             return
         }
         
         let dataTask: URLSessionDataTaskProtocol = urlSession.dataTask(request: urlRequest) { data, response, _ in
-            self.handleResponse(response: response, data: data) { result in
+            self.handleResponse(urlRequest: urlRequest, response: response, data: data, cacheWorker: cacheWorker) { result in
                 switch result {
                 case .success(let data):
                     do {
@@ -53,8 +59,14 @@ final class Networker: NetworkerProtocol {
         dataTask.resume()
     }
     
-    private func handleResponse(response: URLResponse?, data: Data?, completion: @escaping ((Result<Data, NetworkerError>) -> Void)) {
-        guard let statusCode: Int = (response as? HTTPURLResponse)?.statusCode else {
+    private func handleResponse(
+        urlRequest: URLRequest,
+        response: URLResponse?,
+        data: Data?,
+        cacheWorker: CacheworkerProtocol,
+        completion: @escaping ((Result<Data, NetworkerError>) -> Void)
+    ) {
+        guard let response = response, let statusCode: Int = (response as? HTTPURLResponse)?.statusCode else {
             completion(.failure(.unknown("HTTPURLResponse is nil")))
             return
         }
@@ -68,6 +80,8 @@ final class Networker: NetworkerProtocol {
             completion(.failure(.unknown("Data is nil")))
             return
         }
+        
+        cacheWorker.storeCachedResponse(response, for: urlRequest, data: data)
         
         completion(.success(data))
     }
