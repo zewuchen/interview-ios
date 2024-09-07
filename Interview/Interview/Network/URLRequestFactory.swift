e935010d085dfe62f76ba0d172747213eaabb7e5
Rafael Felipe <rafael.felipe1989@gmail.com>
2024-09-07 18:15:28 -0300

    Update URLRequestFactory.swift

--- Interview/Interview/Network/URLRequestFactory.swift
+++ Interview/Interview/Network/URLRequestFactory.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 enum URLRequestFactory {
-    static func make(from endpoint: Endpoint, cachePolicy: URLRequest.CachePolicy) -> URLRequest? {
+    static func make(from endpoint: Endpoint) -> URLRequest? {
         let urlString: String = "https://\(endpoint.host)\(endpoint.baseUrl)"
         
         guard let url = URL(string: urlString) else { return nil }
         
         var urlRequest: URLRequest = .init(url: url)
-        urlRequest.cachePolicy = cachePolicy
+        urlRequest.cachePolicy = .returnCacheDataElseLoad
         urlRequest.httpMethod = endpoint.method.rawValue
         
         return urlRequest
